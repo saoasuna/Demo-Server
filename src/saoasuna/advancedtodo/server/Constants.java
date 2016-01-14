1cbb4196271c86ceb6b0a426cc565294ebbe8926
saoasuna <ryanwong@live.ca>
2016-01-13 23:35:42 -0500

    Server to be used in conjunction with AssignmentTracker
    google web application project built in eclipse, google cloud messaging
    server

---
+++ src/saoasuna/advancedtodo/server/Constants.java
@@ -0,0 +1,18 @@
+package saoasuna.advancedtodo.server;
+
+/**
+
+ */
+public interface Constants {
+
+    /**
+     * Google API Console Browser key.
+     */
+	String API_KEY = "AIzaSyDRH-L6SWhRE-mt1JUOBc8vCWMV3lxONYM";
+
+	String FROM = "email";
+	String REG_ID = "regId";
+	String MSG = "msg";
+	String TO = "email2";
+	
+}
---
+++ src/saoasuna/advancedtodo/server/UnregisterServlet.java
@@ -0,0 +1,44 @@
+package saoasuna.advancedtodo.server;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+import saoasuna.advancedtodo.model.Contact;
+import saoasuna.advancedtodo.model.EMFService;
+
+
+/**
+ * @author appsrox.com
+ *
+ */
+@SuppressWarnings("serial")
+public class UnregisterServlet extends HttpServlet {
+	
+	private static final Logger logger = Logger.getLogger(UnregisterServlet.class.getCanonicalName());
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Unregisters a device from the Demo server.");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String email = req.getParameter(Constants.FROM);
+		
+		EntityManager em = EMFService.get().createEntityManager();
+		try {
+			Contact contact = Contact.find(email, em);
+			if (contact != null) {
+				em.remove(contact);
+				logger.log(Level.WARNING, "Unregistered: " + contact.getId());
+			}
+		} finally {
+			em.close();
+		}		
+	}
+	
+}
